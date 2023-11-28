3ab0f2253796eee440d189c3a46e2ef49628dd31
meeramenon07 <46912602+meeramenon07@users.noreply.github.com>
2023-11-28 13:35:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Let's us start the Name the Song Lyrics Challenge !")
+print("Type in the blank lyrics and see if you are as cool as me.")
+print()
+counter = 1
+while True:
+  #print("Say my _______")
+  
+  missingLyrics = input("Say my _______. ")
+  
+  
+  if missingLyrics == "name" or missingLyrics == "Name":
+    print("Well done! It only took you", counter, "attempts. ")
+  else:
+    print("Nope, try again!")
+    counter += 1
+  if missingLyrics == "name" or missingLyrics == "Name":
+    break
+    print("Finally, you got the missing lyrics in", "attempts. ")
+    
+    
+  
+    
+    
+     
+  
+  
+  
+  
+
